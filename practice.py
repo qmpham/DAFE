c43327c33f6bc534b974f7df4a5bf299279815a4
minhquangpham <minhquang@chmtlp31.limsi.fr>
2019-10-28 00:48:13 +0100

    update

--- practice.py
+++ practice.py
@@ -134,7 +134,7 @@ def train(source_file,
   import time
   start = time.time()  
   meta_train_data_flow = iter(_meta_train_forward())
-  meta_test_data_flow = iter(_meta_test_forward())
+  #meta_test_data_flow = iter(_meta_test_forward())
   while True:
     #####Training batch
     loss = next(meta_train_data_flow)    
@@ -146,7 +146,7 @@ def train(source_file,
     # print("model: ", model.trainable_variables[3])
     # print("snapshot: ", snapshots[3])
     #####Testing batch
-    loss = next(meta_test_data_flow)
+    loss = next(meta_train_data_flow)
     weight_reset(snapshots)
     # print("model: ", model.trainable_variables[3])
     # print("snapshot: ", snapshots[3])
