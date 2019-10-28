0425e15fb62473735d2cc57c8bf8a73385e3f1a7
minhquangpham <minhquang@chmtlp31.limsi.fr>
2019-10-28 17:57:22 +0100

    update

--- practice.py
+++ practice.py
@@ -204,7 +204,7 @@ def translate(source_file,
         memory=encoder_outputs,
         memory_sequence_length=source_length)
     decoded = model.decoder.dynamic_decode(
-        lambda ids: [model.labels_inputter({"ids": ids}), tf.tile(source["domain"],[beam_size])],
+        lambda ids: [model.labels_inputter({"ids": ids}), tf.tile(source["domain"][0],[tf.shape(ids)])],
         tf.fill([batch_size], START_OF_SENTENCE_ID),
         end_id=END_OF_SENTENCE_ID,
         initial_state=decoder_state,
