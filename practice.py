6333baecfe1339113a21aa645389856a1cc2a020
minhquangpham <minhquang@chmtlp31.limsi.fr>
2019-10-29 21:58:58 +0100

    debug

--- practice.py
+++ practice.py
@@ -49,10 +49,10 @@ def merge_map_fn(*args):
   print(src_batches[0].keys())
   for feature in list(src_batches[0].keys()):
     if tf.rank(src_batches[0][feature])==1:
-      print(1,feature)
+      print(1,feature, tf.rank(src_batches[0][feature]))
       src_batch.get(feature, tf.concat([b[feature] for b in src_batches],0))
     elif tf.rank(src_batches[0][feature])==2:
-      print(2,feature)
+      print(2,feature,tf.rank(src_batches[0][feature]))
       len_max = tf.reduce_max(tf.concat([tf.shape(batch[feature])[1] for batch in src_batches]))
       if src_batches[0][feature].dtype == tf.string:
         src_batch.get(feature, tf.concat([tf.concat([batch[feature], tf.fill([tf.shape(batch[feature])[0], len_max-tf.shape(batch[feature])[1]],"")],0) for batch in src_batches],0))
