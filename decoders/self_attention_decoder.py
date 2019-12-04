3a4c6571e3e2ac01dd7fd18452f30de92839c0af
NonameAuPlatal <phamminhquangnt07@gmail.com>
2019-12-04 16:54:16 +0100

    debug

--- decoders/self_attention_decoder.py
+++ decoders/self_attention_decoder.py
@@ -398,9 +398,9 @@ class Multi_domain_SelfAttentionDecoder_v2(Decoder):
           training=training)
       new_cache.append(layer_cache)
       if self.ADAP_layer_stopping_gradient:
-        inputs = multi_domain_layer(tf.stop_gradient(inputs), domain, training=training) + inputs
+        inputs = multi_domain_layer(tf.stop_gradient(inputs), domain, mask=mask, training=training) + inputs
       else:
-        inputs = multi_domain_layer(inputs, domain, training=training) + inputs
+        inputs = multi_domain_layer(inputs, domain, mask=mask, training=training) + inputs
 
     outputs = self.layer_norm(inputs)
     return outputs, new_cache, attention
@@ -538,9 +538,9 @@ class Multi_domain_SelfAttentionDecoder_v2(Decoder):
           training=training)
       new_cache.append(layer_cache)
       if self.ADAP_layer_stopping_gradient:
-        inputs = multi_domain_layer.forward_fn(tf.stop_gradient(inputs), args_dict, domain, training=training) + inputs
+        inputs = multi_domain_layer.forward_fn(tf.stop_gradient(inputs), args_dict, domain, mask=mask, training=training) + inputs
       else:
-        inputs = multi_domain_layer.forward_fn(inputs, args_dict, domain, training=training) + inputs
+        inputs = multi_domain_layer.forward_fn(inputs, args_dict, domain, mask=mask, training=training) + inputs
 
     outputs = self.layer_norm.forward_fn(inputs, args_dict)
     return outputs, new_cache, attention
