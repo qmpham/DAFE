4115dc7d31a237c146c4665ed3fe72d4da16c0ce
NonameAuPlatal <phamminhquangnt07@gmail.com>
2019-12-04 17:13:08 +0100

    debug

--- layers/layers.py
+++ layers/layers.py
@@ -76,9 +76,9 @@ class Multi_domain_FeedForwardNetwork_v2(tf.keras.layers.Layer):
     self.outer_kernel = self.add_weight("%s_outer_weight"%scope_name, shape=[self.domain_numb, self.inner_dim*self.output_dim])
     self.outer_bias = self.add_weight("%s_outer_bias"%scope_name, shape=[self.domain_numb, self.output_dim])
     
-  def call(self, inputs, domain, mask, training=None):  # pylint: disable=arguments-differ
+  def call(self, inputs, domain, mas=None, training=None):  # pylint: disable=arguments-differ
     """Runs the layer."""
-    #mask=tf.cast(mask,tf.float32)
+    mask=tf.cast(mask,tf.float32)
     inputs = self.layer_norm(inputs)
     ##### inner layer
     shape = shape_list(inputs)
@@ -109,8 +109,8 @@ class Multi_domain_FeedForwardNetwork_v2(tf.keras.layers.Layer):
       outputs = tf.nn.bias_add(outputs, dom_outer_bias)
     if self.outer_activation is not None:
       outputs = self.outer_activation(outputs)  # pylint: disable=not-callable
-    #self.add_loss(tf.divide(tf.reduce_sum(mask * tf.reduce_sum(tf.abs(outputs),axis=-1)), tf.reduce_sum(mask)))
-    self.add_loss(tf.reduce_mean(tf.reduce_sum(tf.abs(outputs),axis=-1)))
+    self.add_loss(tf.divide(tf.reduce_sum(mask * tf.reduce_sum(tf.abs(outputs),axis=-1)), tf.reduce_sum(mask)))
+    #self.add_loss(tf.reduce_mean(tf.reduce_sum(tf.abs(outputs),axis=-1)))
     if rank > 2:
       outputs = tf.reshape(outputs, shape[:-1] + [self.output_dim])   
     
