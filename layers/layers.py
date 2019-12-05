e8613e791d33e6709d6b3aecf9f4517e41a960f5
minhquangpham <minhquang@chmtlp31.limsi.fr>
2019-12-05 11:22:50 +0100

    debug

--- layers/layers.py
+++ layers/layers.py
@@ -78,7 +78,8 @@ class Multi_domain_FeedForwardNetwork_v2(tf.keras.layers.Layer):
     
   def call(self, inputs, domain, mask=None, training=None):  # pylint: disable=arguments-differ
     """Runs the layer."""
-    mask=tf.cast(mask,tf.float32)
+    if not(mask is None):
+      mask=tf.cast(mask,tf.float32)
     inputs = self.layer_norm(inputs)
     ##### inner layer
     shape = shape_list(inputs)
@@ -109,8 +110,10 @@ class Multi_domain_FeedForwardNetwork_v2(tf.keras.layers.Layer):
       outputs = tf.nn.bias_add(outputs, dom_outer_bias)
     if self.outer_activation is not None:
       outputs = self.outer_activation(outputs)  # pylint: disable=not-callable
-    self.add_loss(tf.divide(tf.reduce_sum(mask * tf.reduce_sum(tf.abs(outputs),axis=-1)), tf.reduce_sum(mask)))
-    #self.add_loss(tf.reduce_mean(tf.reduce_sum(tf.abs(outputs),axis=-1)))
+    if mask is not None:
+      self.add_loss(tf.divide(tf.reduce_sum(mask * tf.reduce_sum(tf.abs(outputs),axis=-1)), tf.reduce_sum(mask)))
+    else:
+      self.add_loss(tf.reduce_mean(tf.reduce_sum(tf.abs(outputs),axis=-1)))
     if rank > 2:
       outputs = tf.reshape(outputs, shape[:-1] + [self.output_dim])   
     
