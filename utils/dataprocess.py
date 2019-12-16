3f3d58fe1e23197a27c58f40720cdaa3acc65b32
minhquangpham <minhquang@chmtlp31.limsi.fr>
2019-12-16 12:21:36 +0100

    debug

--- utils/dataprocess.py
+++ utils/dataprocess.py
@@ -146,12 +146,12 @@ def create_multi_domain_meta_trainining_dataset(strategy, model, domain, source_
     import itertools
     datasets_size = [count_lines(src) for src in source_file]
     picking_prob_ = [data_size/sum(datasets_size) for data_size in datasets_size]
-    def anneal(step, end=200000):
-      power = (end-step)/end
-      probs = [p**power for p in picking_prob_]
-      return [p/sum(probs) for p in probs]
-    step = tf.data.Dataset.range(200000)
-    picking_prob = step.map(lambda i: anneal(i))
+    def anneal(i, end=200000):
+      i = (end-i)/end
+      prob_ = [p**i  for p in picking_prob_]
+      return [p/sum(prob_) for p in prob_]
+    tensor = tf.Variable(np.array([anneal(i) for i in range(200000)]))
+    picking_prob = tf.data.Dataset.from_tensor_slices(tensor)
     print("picking probability: ", picking_prob)
 
   meta_train_dataset = tf.data.experimental.sample_from_datasets(meta_train_datasets, weights=picking_prob) #tf.data.Dataset.zip(tuple(meta_train_datasets)).map(merge_map_fn) #tf.data.experimental.sample_from_datasets(meta_train_datasets)
