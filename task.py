174fc32465c1f0b3b7069cf48c835cfd1b22fcd5
minhquangpham <minhquang@chmtlp31.limsi.fr>
2019-12-16 15:46:11 +0100

    debug

--- task.py
+++ task.py
@@ -165,7 +165,7 @@ def debug(config,
 
   def _accumulate_gradients(meta_train_source, meta_train_target, meta_test_source, meta_test_target):
     num_examples = tf.reduce_sum(meta_train_target["length"])
-    domain = meta_train_source["domain"][0]
+    domain = meta_test_source["domain"][0]
     tf.print("domain:",domain)
     #tf.print("token_numb:____", num_examples, "domain:____", meta_train_source["domain"][0])
     reported_loss = 0
