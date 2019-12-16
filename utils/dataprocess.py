b04aeadbc24c2be2bb5ba3eb37e61d67da1babdd
minhquangpham <minhquang@chmtlp31.limsi.fr>
2019-12-16 10:32:50 +0100

    debug

--- utils/dataprocess.py
+++ utils/dataprocess.py
@@ -157,7 +157,7 @@ def create_multi_domain_meta_trainining_dataset(strategy, model, domain, source_
     print("picking probability: ", picking_prob)
 
   meta_train_dataset = tf.data.experimental.sample_from_datasets(meta_train_datasets, weights=picking_prob) #tf.data.Dataset.zip(tuple(meta_train_datasets)).map(merge_map_fn) #tf.data.experimental.sample_from_datasets(meta_train_datasets)
-  meta_test_dataset = tf.data.experimental.sample_from_datasets(meta_test_datasets, weights=picking_prob) #tf.data.Dataset.zip(tuple(meta_test_datasets)).map(merge_map_fn)
+  meta_test_dataset = tf.data.experimental.sample_from_datasets(meta_test_datasets, weights=None) #tf.data.Dataset.zip(tuple(meta_test_datasets)).map(merge_map_fn)
   
   with strategy.scope():    
     base_dataset = meta_train_dataset      
