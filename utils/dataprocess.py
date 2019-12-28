23c1d8292c082f1a4fe751ee5f7c7906b5ba97a2
minhquangpham <minhquang@chmtlp31.limsi.fr>
2019-12-28 22:32:49 +0100

    debug

--- utils/dataprocess.py
+++ utils/dataprocess.py
@@ -367,19 +367,20 @@ def create_trainining_dataset_v2(strategy, model, domain, source_file, target_fi
 
 def create_multi_domain_meta_trainining_dataset_v2(strategy, model, domain, source_file, target_file, batch_meta_train_size, batch_meta_test_size, batch_type, shuffle_buffer_size, maximum_length, picking_prob=None):
   meta_train_datasets = [] 
+  meta_train_base_datasets = []
   print("batch_type: ", batch_type)
-  for i, src, tgt in zip(domain, source_file, target_file):
-    meta_train_dataset = model.examples_inputter.make_training_dataset(src, tgt,
-              batch_size=batch_meta_train_size,
-              batch_type=batch_type,
-              batch_multiplier=1,
-              domain=i,
-              shuffle_buffer_size=shuffle_buffer_size,
-              length_bucket_width=1,  # Bucketize sequences by the same length for efficiency.
-              maximum_features_length=maximum_length,
-              maximum_labels_length=maximum_length)
-    
-    with strategy.scope():    
+  with strategy.scope():
+    for i, src, tgt in zip(domain, source_file, target_file):
+      meta_train_dataset = model.examples_inputter.make_training_dataset(src, tgt,
+                batch_size=batch_meta_train_size,
+                batch_type=batch_type,
+                batch_multiplier=1,
+                domain=i,
+                shuffle_buffer_size=shuffle_buffer_size,
+                length_bucket_width=1,  # Bucketize sequences by the same length for efficiency.
+                maximum_features_length=maximum_length,
+                maximum_labels_length=maximum_length)    
+        
       meta_train_base_dataset = meta_train_dataset      
       meta_train_dataset = strategy.experimental_distribute_datasets_from_function(
           lambda _: meta_train_base_dataset)
