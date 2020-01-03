1366fc70eea05a9475cae351c68574edb4458cd9
NonameAuPlatal <phamminhquangnt07@gmail.com>
2020-01-03 12:53:51 +0100

    debug

--- encoders/self_attention_encoder.py
+++ encoders/self_attention_encoder.py
@@ -10,7 +10,7 @@ from opennmt.layers.position import SinusoidalPositionEncoder
 from opennmt.layers import common
 from layers.common import LayerNorm
 from utils.utils_ import make_domain_mask
-from layers.layers import Multi_domain_FeedForwardNetwork, Multi_domain_FeedForwardNetwork_v2, DAFE
+from layers.layers import Multi_domain_FeedForwardNetwork, Multi_domain_FeedForwardNetwork_v2, Multi_domain_FeedForwardNetwork_v3, DAFE, Multi_domain_Gate
 class Multi_domain_SelfAttentionEncoder(Encoder):
 
   def __init__(self,
@@ -111,7 +111,7 @@ class Multi_domain_SelfAttentionEncoder_v2(Encoder):
                ffn_dropout=0.1,
                ffn_activation=tf.nn.relu,
                position_encoder_class=SinusoidalPositionEncoder,
-               multi_domain_adapter_class=Multi_domain_FeedForwardNetwork_v2,
+               multi_domain_adapter_class=Multi_domain_FeedForwardNetwork_v3,
                ADAP_contribution=None,
                **kwargs):
     
@@ -314,7 +314,7 @@ class Multi_domain_SelfAttentionEncoder_v0(Encoder):
       inputs = self.position_encoder(inputs)
     inputs = common.dropout(inputs, self.dropout, training=training)
     mask = self.build_mask(inputs, sequence_length=sequence_length)
-    for i, (layer, multi_domain_layer) in enumerate(zip(self.layers, self.multi_domain_layers)):
+    for layer, multi_domain_layer in zip(self.layers, self.multi_domain_layers):
       inputs = layer(inputs, mask=mask, training=training)
       inputs = multi_domain_layer(inputs, domain, mask=mask, training=training)
     outputs = self.layer_norm(inputs)
@@ -333,7 +333,101 @@ class Multi_domain_SelfAttentionEncoder_v0(Encoder):
     #for layer in self.layers:
     for layer, multi_domain_layer in zip(self.layers,self.multi_domain_layers):
       inputs = layer.forward_fn(inputs, args_dict, mask=mask, training=training)
-      inputs = multi_domain_layer.forward_fn(inputs, args_dict, domain, mask=mask, training=training) + inputs
+      inputs = multi_domain_layer.forward_fn(inputs, args_dict, domain, mask=mask, training=training)
+    outputs = self.layer_norm.forward_fn(inputs, args_dict)
+    return outputs, None, sequence_length
+    
+  def map_v1_weights(self, weights):
+    m = []
+    m += self.layer_norm.map_v1_weights(weights["LayerNorm"])
+    for i, layer in enumerate(self.layers):
+      m += layer.map_v1_weights(weights["layer_%d" % i])
+    return m
+
+class Multi_domain_SelfAttentionEncoder_v1(Encoder):
+
+  def __init__(self,
+               num_layers,
+               num_domains=6,
+               num_domain_units=128,
+               ADAP_layer_stopping_gradient=False,
+               num_units=512,
+               num_heads=8,
+               ffn_inner_dim=2048,
+               dropout=0.1,
+               attention_dropout=0.1,
+               ffn_dropout=0.1,
+               ffn_activation=tf.nn.relu,
+               position_encoder_class=SinusoidalPositionEncoder,
+               multi_domain_adapter_class=Multi_domain_FeedForwardNetwork_v3,
+               multi_domain_adapter_gate_class=Multi_domain_Gate,
+               ADAP_contribution=None,
+               **kwargs):
+    
+    super(Multi_domain_SelfAttentionEncoder_v1, self).__init__(**kwargs)
+    self.num_units = num_units
+    self.dropout = dropout
+    self.position_encoder = None
+    if position_encoder_class is not None:
+      self.position_encoder = position_encoder_class()
+    self.layer_norm = LayerNorm()
+    self.layers = [
+        transformer.SelfAttentionEncoderLayer(
+            num_units,
+            num_heads,
+            ffn_inner_dim,
+            dropout=dropout,
+            attention_dropout=attention_dropout,
+            ffn_dropout=ffn_dropout,
+            ffn_activation=ffn_activation)
+        for i in range(num_layers)]    
+    self.multi_domain_layers = [
+        multi_domain_adapter_class(num_units, num_domain_units, num_units, domain_numb=num_domains, name="ADAP_%d"%i)
+        for i in range(num_layers)]
+    self.multi_domain_gates = [
+        multi_domain_adapter_gate_class(num_units, num_units, domain_numb=num_domains, name="ADAP_gate_%d"%i)
+        for i in range(num_layers)]
+    self.ADAP_layer_stopping_gradient = ADAP_layer_stopping_gradient
+    if ADAP_contribution == None:
+      ADAP_contribution = [1.0] * num_layers
+    self.ADAP_contribution = ADAP_contribution
+  def call(self, inputs, sequence_length=None, training=None):
+    domain = inputs[1]
+    domain = domain[0]
+    inputs = inputs[0]
+    inputs *= self.num_units**0.5
+
+    if self.position_encoder is not None:
+      inputs = self.position_encoder(inputs)
+    inputs = common.dropout(inputs, self.dropout, training=training)
+    mask = self.build_mask(inputs, sequence_length=sequence_length)
+    for layer, multi_domain_layer, multi_domain_gate in zip(self.layers, self.multi_domain_layers, self.multi_domain_gates):
+      inputs = layer(inputs, mask=mask, training=training)
+      g = multi_domain_gate(inputs, domain, mask=mask, training=training)
+      if self.ADAP_layer_stopping_gradient:        
+        inputs = multi_domain_layer(tf.stop_gradient(inputs), domain, mask=mask, training=training) * g + inputs * (1-g)
+      else:
+        inputs = multi_domain_layer(inputs, domain, mask=mask, training=training) * g + inputs * (1-g)
+    outputs = self.layer_norm(inputs)
+    
+    return outputs, None, sequence_length
+
+  def forward_fn(self, inputs, args_dict, sequence_length=None, training=None):
+    domain = inputs[1]
+    domain = domain[0]
+    inputs = inputs[0]
+    inputs *= self.num_units**0.5
+    if self.position_encoder is not None:
+      inputs = self.position_encoder(inputs)
+    inputs = common.dropout(inputs, self.dropout, training=training)
+    mask = self.build_mask(inputs, sequence_length=sequence_length)
+    for layer, multi_domain_layer, multi_domain_gate in zip(self.layers,self.multi_domain_layers,self.multi_domain_gates):
+      inputs = layer.forward_fn(inputs, args_dict, mask=mask, training=training)
+      g = multi_domain_gate.forward_fn(inputs, domain, mask=mask, training=training)
+      if self.ADAP_layer_stopping_gradient:
+        inputs = multi_domain_layer.forward_fn(tf.stop_gradient(inputs), args_dict, domain, mask=mask, training=training) * g + inputs * (1-g)
+      else:
+        inputs = multi_domain_layer.forward_fn(inputs, args_dict, domain, mask=mask, training=training) * g + inputs * (1-g)
     outputs = self.layer_norm.forward_fn(inputs, args_dict)
     return outputs, None, sequence_length
     
--- layers/layers.py
+++ layers/layers.py
@@ -423,6 +423,76 @@ class Multi_domain_FeedForwardNetwork_v1(tf.keras.layers.Layer):
       outputs = tf.reshape(outputs, shape[:-1] + [self.output_dim])
     return outputs
 
+class Multi_domain_Gate(tf.keras.layers.Layer):
+
+  def __init__(self,
+               input_dim, 
+               output_dim,
+               domain_numb=6,
+               dropout=0.1,
+               activation=tf.nn.sigmoid,
+               **kwargs):
+    
+    super(Multi_domain_Gate, self).__init__(**kwargs)
+    self.dropout = dropout
+    self.domain_numb = domain_numb
+    self.input_dim = input_dim
+    self.output_dim = output_dim
+    self.layer_norm = common.LayerNorm()
+    self.inner_layer_norm = common.LayerNorm()
+    self.outer_transpose = False
+    self.outer_use_bias = True
+    self.outer_activation = activation
+  
+  def build(self, input_shape):
+    super(Multi_domain_Gate, self).build(input_shape)
+    scope_name = self.name_scope()
+    self.outer_kernel = self.add_weight("%s_outer_weight"%scope_name, shape=[self.domain_numb, self.input_dim*self.output_dim])
+    self.outer_bias = self.add_weight("%s_outer_bias"%scope_name, shape=[self.domain_numb, self.output_dim])
+    
+  def call(self, inputs, domain, mask=None, training=None):  # pylint: disable=arguments-differ
+    """Runs the layer."""
+    shape = shape_list(inputs)
+    rank = len(shape)      
+    if rank > 2:
+      inputs = tf.reshape(inputs, [-1, shape[-1]])
+    dom_outer_kernel = tf.nn.embedding_lookup(self.outer_kernel, domain)
+    dom_outer_bias = tf.nn.embedding_lookup(self.outer_bias, domain)
+    dom_outer_kernel = tf.reshape(dom_outer_kernel, [-1, self.output_dim])
+    outputs = tf.matmul(inputs, dom_outer_kernel, transpose_b=self.outer_transpose)
+    if self.outer_use_bias:
+      outputs = tf.nn.bias_add(outputs, dom_outer_bias)
+    outputs = self.layer_norm(outputs)
+    if self.outer_activation is not None:
+      outputs = self.outer_activation(outputs)  # pylint: disable=not-callable
+    if rank > 2:
+      outputs = tf.reshape(outputs, shape[:-1] + [self.output_dim])   
+    return outputs
+
+  def forward_fn(self, inputs, args_dict, domain, mask=None, training=None):  # pylint: disable=arguments-differ
+    """Runs the layer."""
+    
+    outer_kernel = args_dict[self.outer_kernel.name]
+    outer_bias = args_dict[self.outer_bias.name]
+    inputs = self.layer_norm(inputs)
+    ##### inner layer
+    shape = shape_list(inputs)
+    rank = len(shape)      
+    if rank > 2:
+      inputs = tf.reshape(inputs, [-1, shape[-1]])
+    dom_outer_kernel = tf.nn.embedding_lookup(outer_kernel, domain)
+    dom_outer_bias = tf.nn.embedding_lookup(outer_bias, domain)
+    dom_outer_kernel = tf.reshape(dom_outer_kernel, [-1, self.output_dim])
+    outputs = tf.matmul(inputs, dom_outer_kernel, transpose_b=self.outer_transpose)
+    if self.outer_use_bias:
+      outputs = tf.nn.bias_add(outputs, dom_outer_bias)
+    outputs = self.layer_norm(outputs)
+    if self.outer_activation is not None:
+      outputs = self.outer_activation(outputs)  # pylint: disable=not-callable
+    if rank > 2:
+      outputs = tf.reshape(outputs, shape[:-1] + [self.output_dim])
+    return outputs
+
 class DAFE(tf.keras.layers.Layer):
 
   def __init__(self,
