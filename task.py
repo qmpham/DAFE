d449679dac3c19a779e60b34d53669d33b66535a
NonameAuPlatal <phamminhquangnt07@gmail.com>
2020-01-29 18:04:11 +0100

    debug

--- task.py
+++ task.py
@@ -3918,6 +3918,16 @@ def train_v13(config,
         training_loss += layer_activity_regularization_loss_scale * tf.add_n(layer_activity_regularization_losses)
       if len(output_activity_regularization_losses)>0:
         training_loss += output_activity_regularization_loss_scale * tf.add_n(output_activity_regularization_losses)
+    
+    if config.get("input_gate_regularizing",False):
+      regularization_losses = model.losses
+      output_activity_regularization_losses = []
+      for loss_ in regularization_losses:
+        if "input_gate" in loss_.name:
+          output_activity_regularization_losses.append(loss_)
+      print(output_activity_regularization_losses)
+      training_loss -= tf.add_n(output_activity_regularization_losses) * config.get("input_gate_regularization_scale", 0.01)
+
     variables = model.trainable_variables
     print("var numb: ", len(variables))
     gradients = optimizer.get_gradients(training_loss, variables)
