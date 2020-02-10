febec6329e00bfa1e33dc484796aea6e4961fae7
NonameAuPlatal <phamminhquangnt07@gmail.com>
2020-02-10 16:50:49 +0100

    debug

--- practice.py
+++ practice.py
@@ -474,8 +474,8 @@ def main():
         ffn_dropout=0.1))
   elif experiment=="DC":
     model = LDR_SequenceToSequence(
-    source_inputter=DC_inputter(embedding_size=config.get("ldr_embedding_size",508), num_domains=config.get("num_domains", 6), num_domain_units=config.get("num_domain_units", 4)),
-    target_inputter=DC_inputter(embedding_size=config.get("ldr_embedding_size",508), num_domains=config.get("num_domains", 6), num_domain_units=config.get("num_domain_units", 4)),
+    source_inputter=DC_inputter(embedding_size=config.get("ldr_embedding_size",508), num_domains=config.get("num_domains", 6), num_domain_units=config.get("num_embedding_domain_units", 4)),
+    target_inputter=DC_inputter(embedding_size=config.get("ldr_embedding_size",508), num_domains=config.get("num_domains", 6), num_domain_units=config.get("num_embedding_domain_units", 4)),
     encoder=onmt.encoders.self_attention_encoder.SelfAttentionEncoder(
         num_layers=6,
         num_units=512,
