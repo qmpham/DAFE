a250520e27d42c0bc2aef593bafdbec2288bcae2
minhquangpham <minhquang@chmtlp31.limsi.fr>
2020-03-29 15:40:06 +0200

    debug

--- cluster_predict.py
+++ cluster_predict.py
@@ -11,7 +11,7 @@ def kmeans_clustering(emb_files, n_clusters, kmeans_save_path, labels_ouput_path
   X = np.concatenate(emb_list,0)
   print("Input shape: ", X.shape)
   print("n_cluster: ", n_clusters)
-  with np.load(kmeans_save_path) as data:
+  with np.load(kmeans_save_path, allow_pickle=True) as data:
     kmeans = KMeans(n_clusters=n_clusters, init='k-means++', n_init=10, max_iter=300, tol=0.0001, precompute_distances='auto', verbose=0, random_state=None, copy_x=True, n_jobs=-1, algorithm='auto')
     kmeans.set_params(**data)
 
@@ -23,7 +23,6 @@ def kmeans_clustering(emb_files, n_clusters, kmeans_save_path, labels_ouput_path
 
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("--src")
 parser.add_argument("--emb_files", nargs="+")
 parser.add_argument("--n_clusters", default=30)
 parser.add_argument("--kmeans_save_path")
