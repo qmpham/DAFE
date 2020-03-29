ea6c610b95ff44efebc3775017160cf072c4133a
minhquangpham <minhquang@chmtlp31.limsi.fr>
2020-03-29 15:53:05 +0200

    debug

--- kmeans.py
+++ kmeans.py
@@ -14,7 +14,7 @@ def kmeans_clustering(emb_files, n_clusters, kmeans_save_path, labels_ouput_path
   kmeans = KMeans(n_clusters=n_clusters, init='k-means++', n_init=10, max_iter=300, tol=0.0001, precompute_distances='auto', verbose=0, random_state=None, copy_x=True, n_jobs=-1, algorithm='auto').fit(X)
 
   label_predictions = kmeans.predict(X)
-  kmeans_params = kmeans.get_params()
+  kmeans_params = kmeans.get_params(deep=False)
   np.savez(kmeans_save_path, **kmeans_params)
   with open(labels_ouput_path, "w") as f:
     for l in label_predictions:
