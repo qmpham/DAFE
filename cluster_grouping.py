fdf83788ed2d715dec0fda06b6071e3f37a0e9c9
minhquangpham <minhquang@chmtlp31.limsi.fr>
2020-03-29 14:48:25 +0200

    debug

--- cluster_grouping.py
+++ cluster_grouping.py
@@ -17,15 +17,17 @@ tgt_files = [open(tgt_path+".cluster.%d"%i,"w") for i in range(n_clusters)]
 src_tag_files = [open(src_path+".cluster.%d.tagged"%i,"w") for i in range(n_clusters)]
 tgt_tag_files = [open(tgt_path+".cluster.%d.tagged"%i,"w") for i in range(n_clusters)]
 
-with open(path,"r") as f1:
-    with open(label_path,"r") as f2:
-        ls = [l.strip() for l in f1.readlines()]
-        domains = [int(l.strip()) for l in f2.readlines()]
-        for l, domain in zip(ls,domains):
-            print(l,file=src_files[domain])
-            print(l,file=tgt_files[domain])
-            print("Domain=%d %s"%(domain,l),file=src_tag_files[domain])
-            print("Domain=%d %s"%(domain,l),file=tgt_tag_files[domain])
+with open(src_path,"r") as f1:
+    with open(tgt_path, "r") as f3:
+        with open(label_path,"r") as f2:
+            src_ls = [l.strip() for l in f1.readlines()]
+            tgt_ls = [l.strip() for l in f3.readlines()]
+            domains = [int(l.strip()) for l in f2.readlines()]
+            for l, domain in zip(src_ls, tgt_ls, domains):
+                print(l,file=src_files[domain])
+                print(l,file=tgt_files[domain])
+                print("Domain=%d %s"%(domain,l),file=src_tag_files[domain])
+                print("Domain=%d %s"%(domain,l),file=tgt_tag_files[domain])
 
 [f.close() for f in src_files]
 [f.close() for f in tgt_files]
