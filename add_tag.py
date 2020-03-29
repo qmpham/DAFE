a6617d724c7a10f208401d5359758aa073e12fc0
minhquangpham <minhquang@chmtlp31.limsi.fr>
2020-03-29 23:56:14 +0200

    debug

---
+++ add_tag.py
@@ -0,0 +1,9 @@
+import sys
+
+with open(sys.argv[1],"r") as f_r:
+    with open(sys.argv[2],"r") as f_tag:
+        with open(sys.argv[1]+".30.tagged","w") as f_w:
+            ls = [l.strip() for l in f_r.readlines()]
+            tags = [int(t.strip()) for t in f_tag.readlines()]
+            for l, tag in zip(ls,tags):
+                print("Domain=%d %s"%(tag,l),file=f_w)
