82ed74662f54fbdfd2305c6df10b7420a5aa4ff5
minhquangpham <minhquang@chmtlp31.limsi.fr>
2020-03-29 23:57:52 +0200

    debug

--- add_tag.py
+++ add_tag.py
@@ -2,7 +2,7 @@ import sys
 
 with open(sys.argv[1],"r") as f_r:
     with open(sys.argv[2],"r") as f_tag:
-        with open(sys.argv[1]+".30.tagged","w") as f_w:
+        with open(sys.argv[1]+".30.clusters.tagged","w") as f_w:
             ls = [l.strip() for l in f_r.readlines()]
             tags = [int(t.strip()) for t in f_tag.readlines()]
             for l, tag in zip(ls,tags):
