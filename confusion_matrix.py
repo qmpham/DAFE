76ce5c539ca239c6ddf0545ac9ef491f35a881c5
minhquangpham <minhquang@chmtlp31.limsi.fr>
2020-03-31 14:57:27 +0200

    debug

---
+++ confusion_matrix.py
@@ -0,0 +1,21 @@
+import argparse
+import numpy as np
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--label",nargs="+")
+parser.add_argument("--n_clusters")
+
+args = parser.parse_args()
+
+n_clusters = int(args.n_clusters)
+label_paths = args.label
+domain_numb = len(label_path)
+confusion_matrix = np.zeros([domain_numb, n_clusters])
+
+for i in range(domain_numb):
+    label_path = label_paths[i]
+    with open(label_path,"r") as f2:
+        domains = [int(l.strip()) for l in f2.readlines()]
+        for domain in domains:
+            confusion_matrix[i, domain] += 1
+
+print(confusion_matrix/np.sum(confusion_matrix,0).reshape(1,-1))
