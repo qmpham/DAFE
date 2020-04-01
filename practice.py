c60a585e1f65053d32e086d304af790067956f26
minhquangpham <minhquang@chmtlp31.limsi.fr>
2020-04-01 13:06:10 +0200

    debug

--- practice.py
+++ practice.py
@@ -917,7 +917,7 @@ def main():
   learning_rate = onmt.schedules.ScheduleWrapper(schedule=onmt.schedules.NoamDecay(scale=1.0, model_dim=512, warmup_steps=warmup_steps), step_duration= config.get("step_duration",16))
   meta_train_optimizer = tf.keras.optimizers.SGD(0.0001)
   adv_optimizer = tfa.optimizers.LazyAdam(0.0001)
-  meta_test_optimizer = tfa.optimizers.LazyAdam(1.0)
+  meta_test_optimizer = tfa.optimizers.LazyAdam(learning_rate)
   checkpoint = tf.train.Checkpoint(model=model, optimizer=meta_test_optimizer)   
   model.initialize(data_config)
   checkpoint_manager = tf.train.CheckpointManager(checkpoint, config["model_dir"], max_to_keep=5)
