c7df00d03d845ae12350a4c741e695aabfd1b945
minhquangpham <minhquang@chmtlp31.limsi.fr>
2020-04-07 12:24:55 +0200

    debug

--- utils/utils_.py
+++ utils/utils_.py
@@ -106,6 +106,10 @@ def load_and_update_if_needed_from_ckpt(model_dir,
         print(variable.name)
         new_value = np.concatenate((value, np.zeros((1))),axis=0)
         variable.assign(new_value)
+      elif vocab_update and "dense_96/kernel" in variable.name:
+        print(variable.name)
+        new_value = np.concatenate((value, np.zeros((1,512))),axis=1)
+        variable.assign(new_value)
       else:
         print(variable.name)
         variable.assign(value)
