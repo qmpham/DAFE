656cf12872258090a382e772e6aa406316e2b0db
minhquangpham <minhquang@chmtlp31.limsi.fr>
2020-04-09 17:28:58 +0200

    debug

--- task.py
+++ task.py
@@ -847,7 +847,7 @@ def elastic_finetuning(config,
     load_and_update_if_needed_from_ckpt(config["model_dir"],   
                         checkpoint_path,                        
                         trackables={"model":model},
-                        vocab_update=True,
+                        vocab_update=config.get("vocab_updata",False),
                         model_key="model")
 
   ## assign value to star_vars
