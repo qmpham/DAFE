89cccbe8e1e4663dd14c11098d087e18c06ab641
minhquangpham <minhquang@chmtlp31.limsi.fr>
2020-04-19 18:22:37 +0200

    debug

--- practice.py
+++ practice.py
@@ -29,10 +29,11 @@ from layers.layers import Regulation_Gate, Multi_domain_FeedForwardNetwork_v7, M
 def main():
   seed = 1234
   tf.random.set_seed(seed)
+  physical_devices = tf.config.list_physical_devices('GPU')
+  tf.config.experimental.set_memory_growth(physical_devices[0], enable=True)
   devices = tf.config.experimental.list_logical_devices(device_type="GPU")
-  tf.config.experimental.set_memory_growth(devices[0], enable=True)
   print(devices)
-  strategy = tf.distribute.MirroredStrategy(devices=[d.name for d in devices])
+  strategy = tf.distribute.MirroredStrategy(devices=[d.name for d in physical_devices])
   parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
   parser.add_argument("run", choices=["train", "proxy", "proxy1","translatev7","kmeans", "translatev5", "translatev6","sentence_encode", "train_wdc", "train_denny_britz", "train_ldr", "visualize", "experimental_translate", "trainv3", "dcote", "metatrainv12", "trainv13", "trainv2", "trainv12", "metatrainv15", "translatev1", "trainv8", "translate", "translatev2", "translatev3", "metatrainv9", "metatrainv11", "debug","metatrainv1", "metatrainv2", "metatrainv3", "inspect", "metatrainv5", "metatrainv6", "metatrainv7", "metatrainv8", "metatrainv10", "elastic_finetune", "finetune"], help="Run type.")
   parser.add_argument("--config", help="configuration file")
