698beaa15dcdff5ff516d13daf6acd60d9912162
minhquangpham <minhquang@chmtlp31.limsi.fr>
2020-04-19 18:05:34 +0200

    debug

--- practice.py
+++ practice.py
@@ -847,12 +847,11 @@ def main():
     model = SequenceToSequence(
     source_inputter=WordEmbedder(embedding_size=512),
     target_inputter=WordEmbedder(embedding_size=512),
-    encoder=onmt.encoders.rnn_encoder.RNNEncoder(
+    encoder=onmt.encoders.rnn_encoder.LSTMEncoder(
       num_layers=1,
       num_units=1024,
       bidirectional=True,
       residual_connections=True,
-      cell_class=tf.keras.layers.LSTMCell,
       dropout=0.1),
     decoder=onmt.decoders.rnn_decoder.AttentionalRNNDecoder(
       num_layers=1,
