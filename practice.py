0cc2b31f027ddf141fb6f7a6bd94115307c421e5
minhquangpham <minhquang@chmtlp31.limsi.fr>
2020-06-02 17:20:19 +0200

    debug

--- practice.py
+++ practice.py
@@ -1029,7 +1029,8 @@ def main():
         dropout=0.1,
         attention_dropout=0.1,
         ffn_dropout=0.1,
-        multi_domain_adapter_class=Multi_domain_FeedForwardNetwork_v9),
+        multi_domain_adapter_class=Multi_domain_FeedForwardNetwork_v9,
+        ADAP_contribution=[0.0]*num_domains),
     decoder=Multi_domain_SelfAttentionDecoder_v19(
         num_layers=6,
         num_domains=num_domains,
@@ -1041,7 +1042,8 @@ def main():
         dropout=0.1,
         attention_dropout=0.1,
         ffn_dropout=0.1,
-        multi_domain_adapter_class=Multi_domain_FeedForwardNetwork_v9))
+        multi_domain_adapter_class=Multi_domain_FeedForwardNetwork_v9,
+        ADAP_contribution=[0.0]*num_domains))
   
   elif experiment=="WDC":
     model = SequenceToSequence_WDC(
