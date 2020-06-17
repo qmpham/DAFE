cbb06e662f91fa15ffcc90cb3816a55fafab8a66
minhquangpham <minhquang@chmtlp31.limsi.fr>
2020-06-17 20:02:09 +0200

    debug

--- task.py
+++ task.py
@@ -7315,6 +7315,17 @@ def finetune_wada_v1(config,
     return loss, num_examples
 
   @dataset_util.function_on_next(train_dataset)
+  def _train_model_forward(next_fn):    
+    with strategy.scope():
+      per_replica_source, per_replica_target = next_fn()
+      per_replica_loss, per_replica_num_examples = strategy.experimental_run_v2(
+          _accumulate_model_gradients, args=(per_replica_source, per_replica_target))
+      # TODO: these reductions could be delayed until _step is called.
+      loss = strategy.reduce(tf.distribute.ReduceOp.MEAN, per_replica_loss, None)      
+      num_examples = strategy.reduce(tf.distribute.ReduceOp.SUM, per_replica_num_examples, None)
+    return loss, num_examples
+
+  @dataset_util.function_on_next(train_dataset)
   def _train_iteration(next_fn):    
     with strategy.scope():
       per_replica_source, per_replica_target = next_fn()
