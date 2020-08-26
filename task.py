dc6826c276119c1bba2a3f1aa8f3d49fc9984631
minhquangpham <minhquang@chmtlp31.limsi.fr>
2020-08-26 16:50:57 +0200

    debug

--- task.py
+++ task.py
@@ -7711,7 +7711,225 @@ def score(source_file,
   
   return 0
 
+def EWC_stat(source_file,
+              reference,
+              model,
+              config,
+              strategy,
+              optimizer,
+              checkpoint_manager,
+              checkpoint,              
+              domain,
+              output_file,
+              length_penalty,
+              is_noisy=1,
+              checkpoint_path=None,
+              probs_file=None,
+              experiment="ldr",
+              score_type="MultiBLEU",
+              batch_size=5,
+              beam_size=5):
+  
+  # Create the inference dataset.
+  if checkpoint_path == None:
+    checkpoint_path = checkpoint_manager.latest_checkpoint
+  tf.get_logger().info("Evaluating model %s", checkpoint_path)
+  print("In domain %d"%domain)
+  checkpoint.restore(checkpoint_path)
+  dataset = model.examples_inputter.make_inference_dataset(source_file, 1, domain)
+  iterator = iter(dataset)
+  model.create_variables(optimizer=optimizer)
 
+  @tf.function
+  def star_vars_init():
+    variables = model.trainable_variables
+    with tf.init_scope():
+      for var in variables:
+        value=var.numpy()
+        star_vars.append(tf.constant(value))
+        
+  @tf.function(experimental_relax_shapes=True)
+  def minifinetune(source, target):
+    tf.print("context_src: ", source["tokens"], "context_target: ", target["tokens"])
+    outputs, _ = model(
+        source,
+        labels=target,
+        training=True,
+        step=optimizer.iterations)
+    loss = model.compute_loss(outputs, target, training=True)
+
+    if isinstance(loss, tuple):
+      training_loss = loss[0] / loss[1]
+    else:
+      training_loss, _ = loss, loss        
+    variables = model.trainable_variables
+    gradients = optimizer.get_gradients(training_loss, variables)
+    grads_and_vars = []
+    for gradient, variable in zip(gradients, variables):
+      
+
+  with open(output_file, "w") as output_:
+    while True:    
+      try:
+        
+      except tf.errors.OutOfRangeError:
+        break
+  
+  return 0
+
+def translate_farajan_residual(source_file,
+              context_src_file,
+              context_tgt_file,
+              reference,
+              model,
+              config,
+              strategy,
+              optimizer,
+              checkpoint_manager,
+              checkpoint,              
+              domain,
+              output_file,
+              length_penalty,
+              is_noisy=1,
+              checkpoint_path=None,
+              probs_file=None,
+              experiment="ldr",
+              score_type="MultiBLEU",
+              batch_size=5,
+              beam_size=5):
+  
+  # Create the inference dataset.
+  if checkpoint_path == None:
+    checkpoint_path = checkpoint_manager.latest_checkpoint
+  tf.get_logger().info("Evaluating model %s", checkpoint_path)
+  print("In domain %d"%domain)
+  checkpoint.restore(checkpoint_path)
+  dataset = model.examples_inputter.make_inference_dataset(source_file, 1, domain)
+  iterator = iter(dataset)
+  if "baseline" in experiment:
+    context_dataset = model.examples_inputter.make_training_dataset(context_src_file, context_tgt_file, batch_size=1, batch_type="example")
+  else:
+    context_dataset = model.examples_inputter.make_training_dataset(context_src_file, context_tgt_file, 1, domain, batch_type="example", single_pass=True)
+  context_iteration = iter(context_dataset)
+  ids_to_tokens = model.labels_inputter.ids_to_tokens
+  optimizer = tfa.optimizers.LazyAdam(config.get("farajan_lr",0.001))
+  model.create_variables(optimizer=optimizer)
+  @tf.function(experimental_relax_shapes=True)
+  def minifinetune(source, target):
+    tf.print("context_src: ", source["tokens"], "context_target: ", target["tokens"])
+    outputs, _ = model(
+        source,
+        labels=target,
+        training=True,
+        step=optimizer.iterations)
+    loss = model.compute_loss(outputs, target, training=True)
+
+    if isinstance(loss, tuple):
+      training_loss = loss[0] / loss[1]
+    else:
+      training_loss, _ = loss, loss        
+    #variables = model.trainable_variables
+    variables = []
+    for v in model.trainable_variables:
+      if "ADAP_" in v.name or "ldr_embedding" in v.name or "ldr_inputter" in v.name:
+        print(v.name)
+        variables.append(v)
+    #print("var numb: ", len(variables))
+    gradients = optimizer.get_gradients(training_loss, variables)
+    grads_and_vars = []
+    for gradient, variable in zip(gradients, variables):
+      grads_and_vars.append((gradient, variable))
+    for i in range(config.get("farajan_steps",9)):
+      optimizer.apply_gradients(grads_and_vars)
+
+  @tf.function
+  def predict_next():    
+    source = next(iterator)
+    tf.print("source: ", source["tokens"])
+    #context_src, context_tgt = next(context_iteration)
+    #tf.print("source: ", source, "src_context: ", context_src, "tgt_context: ", context_tgt)
+    source_length = source["length"]
+    batch_size = tf.shape(source_length)[0]
+    source_inputs = model.features_inputter(source)
+    if experiment in ["residual","residualv15","DRO","residualv25","residualv27","residualv28","residualv29","residual_big_transformer","residualv26","gated_residual_v5","residualv16","residualv19","residualv20","residualv21","residualv22","residualv23","residualv17","residualv18","residualv2","residualv1","residualv3","residualv5","residualv13","residualv12","residualv6","residualv7","residualv11","residualv8","residualv9","baselinev1"]:
+      encoder_outputs, _, _ = model.encoder([source_inputs, source["domain"], source["is_noisy"]], source_length, training=False, internal_node_printing=True)
+    else:
+      encoder_outputs, _, _ = model.encoder(source_inputs, source_length, training=False)
+
+    # Prepare the decoding strategy.
+    if beam_size > 1:
+      encoder_outputs = tfa.seq2seq.tile_batch(encoder_outputs, beam_size)
+      source_length = tfa.seq2seq.tile_batch(source_length, beam_size)
+      decoding_strategy = onmt.utils.BeamSearch(beam_size, length_penalty=length_penalty)
+    else:
+      decoding_strategy = onmt.utils.GreedySearch()
+
+    # Run dynamic decoding.
+    decoder_state = model.decoder.initial_state(
+        memory=encoder_outputs,
+        memory_sequence_length=source_length)
+    if experiment in ["residual","residualv2","DRO","residualv15","residualv25","residualv27","residual_big_transformer","residualv26","gated_residual_v5","residualv16","residualv19","residualv20","residualv21","residualv22","residualv23","residualv17","residualv18","residualv1","residualv3","residualv5","residualv6","residualv7","residualv13","residualv12","residualv11","residualv8","residualv9","baselinev1"]:
+      map_input_fn = lambda ids: [model.labels_inputter({"ids": ids}, training=False), tf.dtypes.cast(tf.fill(tf.expand_dims(tf.shape(ids)[0],0), domain), tf.int64)]
+    elif experiment in ["DC"]:
+      map_input_fn = lambda ids: model.labels_inputter({"ids": ids}, domain=domain, training=False)
+    elif experiment in ["WDC"]:
+      e_r, _ = model.classification_layer(encoder_outputs, source_length, training=False)
+      e_s, _ = model.adv_classification_layer(encoder_outputs, source_length, training=False)
+      g_s = model.share_gate(tf.concat([tf.tile(tf.expand_dims(e_s,1),[1,tf.shape(encoder_outputs)[1],1]),encoder_outputs],-1))
+      g_r = model.specific_gate(tf.concat([tf.tile(tf.expand_dims(e_r,1),[1,tf.shape(encoder_outputs)[1],1]),encoder_outputs],-1))
+      h_r = g_r * encoder_outputs
+      h_s = g_s * encoder_outputs
+      encoder_mask = model.encoder.build_mask(source_inputs, sequence_length=source_length)
+      map_input_fn = lambda ids: [model.labels_inputter({"ids": ids}, training=False), h_r, h_s, encoder_mask]
+    elif experiment in ["residualv28","residualv29"]:
+      map_input_fn = lambda ids: [model.labels_inputter({"ids": ids}, training=False), source["domain"]]
+    else:
+      map_input_fn = lambda ids: model.labels_inputter({"ids": ids}, training=False)
+    decoded = model.decoder.dynamic_decode(
+        map_input_fn,
+        tf.fill([batch_size], START_OF_SENTENCE_ID),
+        end_id=END_OF_SENTENCE_ID,
+        initial_state=decoder_state,
+        decoding_strategy=decoding_strategy,
+        maximum_iterations=250)
+    target_lengths = decoded.lengths
+    target_tokens = ids_to_tokens.lookup(tf.cast(decoded.ids, tf.int64)) 
+    return target_tokens, target_lengths
+  
+  def _set_weight(v, w):
+    v.assign(tf.cast(w,v.dtype))
+
+  @tf.function
+  def weight_reset(snapshots):
+    for snap, var in zip(snapshots, model.trainable_variables):
+      _set_weight(var, snap)
+  # Iterates on the dataset.
+
+  print("output file: ", output_file)
+  step = optimizer.iterations.numpy()
+  with open(output_file, "w") as output_:
+    while True:    
+      try:
+        # save values
+        snapshots = [v.value() for v in model.trainable_variables]
+        #finetuning phase
+        src, tgt = next(context_iteration)
+        if src["length"].numpy()>1:
+          minifinetune(src,tgt)
+        #translating phase
+        batch_tokens, batch_length = predict_next()
+        #reset parameters
+        weight_reset(snapshots)
+        #reset step
+        optimizer.iterations.assign(step)
+        for tokens, length in zip(batch_tokens.numpy(), batch_length.numpy()):
+          sentence = b" ".join(tokens[0][:length[0]])
+          print_bytes(sentence, output_)
+          #print_bytes(sentence)
+      except tf.errors.OutOfRangeError:
+        break
+  
+  return 0
 
 
 
