450699a5471035bc2c7d3f508bee4d9dd8edbb6d
minhquangpham <minhquang@chmtlp31.limsi.fr>
2020-09-10 16:12:11 +0200

    debug

--- task.py
+++ task.py
@@ -708,8 +708,9 @@ def elastic_finetuning(config,
     elif elastic_type=="EWC":
       assert EWC_path !=None
       EWC_weights = np.load(EWC_path)
+      EWC_scale = config.get("EWC_scale",1.0)
       for i in range(len(variables)):
-        training_loss += tf.reduce_sum(tf.square(variables[i] - star_vars[i]) * EWC_weights[variables[i].name]) 
+        training_loss += tf.reduce_sum(tf.square(variables[i] - star_vars[i]) * EWC_weights[variables[i].name]) * EWC_scale
     print("var numb: ", len(variables))
     gradients = optimizer.get_gradients(training_loss, variables)
     gradient_accumulator(gradients)
