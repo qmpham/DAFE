75981d50f86e293e068b54874d8e578f037e0d64
minhquangpham <minhquang@chmtlp31.limsi.fr>
2020-11-06 15:32:46 +0100

    debug

--- task.py
+++ task.py
@@ -8231,10 +8231,18 @@ def train_NGD(config,
         step=optimizer.iterations)
     loss = model.compute_individual_loss(outputs, tgt, training=True)
     return loss
+  
   def _accumulate_diag_hessians(source,target):    
+    outputs, _ = model(
+        src,
+        labels=tgt,
+        training=True,
+        step=optimizer.iterations)
+    loss = model.compute_individual_loss(outputs, tgt, training=True)
     variables = model.trainable_variables
-    loss = _build_model(source, target)
-    diag_hessians = []
+    #loss = _build_model(source, target)
+    #diag_hessians = []
+    """
     for var in variables:
       #jacobian = tape.jacobian(loss, var, parallel_iterations=batch_hessian_size, experimental_use_pfor=False) 
       def hessian_accum_along_loss(diag_hessian_approx, x):
@@ -8246,7 +8254,16 @@ def train_NGD(config,
       #jacobian 
       #diag_hessian_approx = tf.reduce_mean(jacobian * jacobian, 0)
       diag_hessians.append(tf.scan(hessian_accum_along_loss, loss, initializer=tf.zeros_like(var), parallel_iterations=batch_hessian_size)[-1]/tf.cast(batch_hessian_size,tf.float32))
-    hessian_accumulators(diag_hessians)
+    """
+    def hessian_accum_along_loss(diag_hessian_acc, x):
+      gradients = optimizer.get_gradients(x,variables)
+      diag_hessian_acc_new = []
+      for grad, acc in zip(gradients, diag_hessian_acc):
+        if isinstance(grad, tf.IndexedSlices):
+          diag_hessian_acc_new.append(tf.tensor_scatter_nd_add(acc, tf.expand_dims(grad.indices,1), grad.values * grad.values))
+        else:
+          return diag_hessian_acc_new.append(tf.add(acc, grad * grad))
+    hessian_accumulators([p[-1]/batch_hessian_size for p in tf.scan(hessian_accum_along_loss, loss, initializer=[tf.zeros_like(var) for var in variables], parallel_iterations=batch_hessian_size)])
   def _accumulate_gradients(source, target):
     outputs, _ = model(
         source,
