d903978fa53217733e1a1858147ee50786aefc8e
minhquangpham <minhquang@chmtlp31.limsi.fr>
2020-11-07 13:11:28 +0100

    debug

--- task.py
+++ task.py
@@ -8219,12 +8219,6 @@ def train_NGD(config,
             tf.zeros_like(var),
             trainable=False, synchronization=tf.VariableSynchronization.ON_READ) for var in model.trainable_variables]
 
-  def normalize_hessian_accumulators():
-    sum = 0
-    for h in hessian_accumulators.hessians:
-      sum += tf.reduce_sum(h).numpy()
-    for h in hessian_accumulators._hessians:
-      h = h/sum
   #########
   
   def _build_model(src,tgt):
@@ -8360,10 +8354,10 @@ def train_NGD(config,
     new_gradients = []
     for gradient, hessian_moving_stat in zip(gradients, hessian_moving_stats):
       if isinstance(gradient,tf.IndexedSlices):
-        new_gradients.append(tf.IndexedSlices(gradient.values / (tf.nn.embedding_lookup(hessian_moving_stat.value(), gradient.indices) + epsilon), 
+        new_gradients.append(tf.IndexedSlices(gradient.values / (tf.nn.embedding_lookup(hessian_moving_stat.value(), gradient.indices) * tf.reduce_sum(hessian_moving_stat.value()) + epsilon), 
         gradient.indices, dense_shape=gradient.dense_shape))
       else:
-        new_gradients.append(gradient / (hessian_moving_stat+epsilon))
+        new_gradients.append(gradient / (hessian_moving_stat+epsilon) * tf.reduce_sum(hessian_moving_stat))
     gradient_accumulator(new_gradients)
     num_examples = tf.reduce_sum(target["length"])
     return reported_loss, num_examples
