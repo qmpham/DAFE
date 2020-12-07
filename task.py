4a763135c28af9502c0e6a61552d778e7ed1f17f
minhquangpham <minhquang@chmtlp31.limsi.fr>
2020-12-07 16:07:09 +0100

    debug

--- task.py
+++ task.py
@@ -8548,7 +8548,7 @@ def train_NGD(config,
     target_scores = config.get("eval_target_scores",None)
     achivement_percentage = [1-e/float(t) for e,t in zip(eval_scores, target_scores)]
     new_picking_prob = [p/sum(achivement_percentage) for p in achivement_percentage]
-    new_picking_prob = [p if not overfitted else 0 for p, overfitted in zip(new_picking_prob, overfitting)]
+    new_picking_prob = [p if not overfitted else data_size/(3*sum(datasets_size)) for p, overfitted, data_size in zip(new_picking_prob, overfitting, datasets_size)]
     new_picking_prob = [p/sum(achivement_percentage) for p in achivement_percentage]
     print("new_picking_prob: ", new_picking_prob)
     train_dataset = create_trainining_dataset(strategy, model, domain, source_file, target_file, batch_train_size, batch_type, shuffle_buffer_size, 
@@ -8629,7 +8629,7 @@ def train_NGD(config,
         target_scores = config.get("eval_target_scores",None)
         achivement_percentage = [1-e/float(t) for e,t in zip(eval_scores, target_scores)]
         new_picking_prob = [p/sum(achivement_percentage) for p in achivement_percentage]
-        new_picking_prob = [p if not overfitted else 0 for p, overfitted in zip(new_picking_prob, overfitting)]
+        new_picking_prob = [p if not overfitted else data_size/(3*sum(datasets_size)) for p, overfitted, data_size in zip(new_picking_prob, overfitting, datasets_size)]
         new_picking_prob = [p/sum(achivement_percentage) for p in achivement_percentage]
         print("new_picking_prob: ", new_picking_prob)
         train_dataset = create_trainining_dataset(strategy, model, domain, source_file, target_file, batch_train_size, batch_type, shuffle_buffer_size, 
