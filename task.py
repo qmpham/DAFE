46869344e9384fe007922461a1d6165e99775009
minhquangpham <phamminhquangnt07@gmail.com>
2021-01-04 15:58:38 +0100

    debug

--- task.py
+++ task.py
@@ -9826,7 +9826,9 @@ def train_L2W(config,
         sampler_optimizer = tf.keras.optimizers.Adam(learning_rate=config.get("sampler_optim_lr",0.01))
         with strategy.scope():
           domain_logits = tf.Variable([1.0]*len(domain), trainable=True)
-          sampler_optimizer._create_slots((domain_logits))
+          trainable_vars = []
+          trainable_vars.append(domain_lists)
+          sampler_optimizer._create_slots(trainable_vars)
         with tf.GradientTape() as tape:
           _actor_loss = - tf.reduce_sum(tf.nn.softmax(domain_logits) * tf.nn.log_softmax(domain_logits) * domain_rewards)
           d_logits_grad = tape.gradient(_actor_loss, domain_logits)
