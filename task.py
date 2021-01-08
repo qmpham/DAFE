64dd546be37ca6397ea642a7cf7961f964069e12
minhquangpham <phamminhquangnt07@gmail.com>
2021-01-08 18:01:20 +0100

    debug

--- task.py
+++ task.py
@@ -9888,14 +9888,24 @@ def train_L2W(config,
         snapshots = [v.value() for v in model.trainable_variables]
         saved_step = optimizer.iterations.numpy()
         #######
+        current_probs = tf.nn.softmax(domain_logits).numpy()
+        print("current_probs: ", current_probs)
         for i, train_iter in enumerate(train_iterators):
           _reward = 0.0
           with strategy.scope():
+            ##### accumulate gradient over training set of src domain i at theta_t
             for _ in range(config.get("train_batch_per_run_num",10)):
               src, tgt = next(train_iter)
               strategy.experimental_run_v2(_accumulate_dev_train_gradients, args=(src, tgt))
             train_gradient_accumulator(sub_gradient_accumulator.gradients)
+            strategy.experimental_run_v2(sub_gradient_accumulator.reset)
+            ##### compute theta_t+1
+            for _ in range(config.get("train_batch_per_run_num",10)):
+              k = np.random.choice(domain, 10, p=current_probs)
+              src, tgt = next(train_iter[k])
+              strategy.experimental_run_v2(_accumulate_dev_train_gradients, args=(src, tgt))
             strategy.experimental_run_v2(_apply_dev_train_gradients)
+            ##### accumulate gradient over dev set of k tgt domains at theta_t+1
             for j, dev_iter in enumerate(dev_iterators):
               _sum = 0.0
               _dev_norm = 0.0
