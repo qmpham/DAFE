e842380ecb00d6b268483adc09a33f956dfa408e
minhquangpham <phamminhquangnt07@gmail.com>
2021-01-20 16:01:24 +0100

    debug

--- task.py
+++ task.py
@@ -9620,7 +9620,9 @@ def train_L2W(config,
   print("domain_importances: ", domain_importances)
 
   def _sampler_loss():
-    _actor_loss = - tf.reduce_sum(tf.stop_gradient(tf.nn.softmax(domain_logits)) * tf.nn.log_softmax(domain_logits) * domain_rewards)
+    _actor_loss = - tf.reduce_sum(tf.stop_gradient(tf.nn.softmax(domain_logits)) * tf.nn.log_softmax(domain_logits) * domain_rewards) 
+    if config.get("sampler_entropy_constraint",False):
+      _actor_loss -=  config.get("sampler_entropy_constraint_weight",1e-3)* tf.nn.log_softmax(domain_logits)
     d_logits_grad = sampler_optimizer.get_gradients(_actor_loss, sampler_vars)
     d_logits_grad_accumulator(d_logits_grad)
     return _actor_loss
