32c5c86b4b4d0887d624837912f228d294dc8639
minhquangpham <phamminhquangnt07@gmail.com>
2021-01-21 11:40:53 +0100

    debug

--- task.py
+++ task.py
@@ -8199,7 +8199,8 @@ def train_NGD(config,
                                             maximum_length, length_bucket_width=config.get("length_bucket_width",1), 
                                             multi_domain=config.get("multi_domain", True), picking_prob=config.get("picking_prob",None), 
                                             temperature=config.get("temperature",1.0))
-  hessian_datasets = create_trainining_dataset(strategy, model, domain, config.get("hessian_src", source_file), config.get("hessian_ref", target_file), batch_hessian_size, "examples", shuffle_buffer_size, 
+  hessian_datasets = create_trainining_dataset(strategy, model, domain, config.get("hessian_src", source_file), 
+                                            config.get("hessian_ref", target_file), batch_hessian_size, "examples", shuffle_buffer_size, 
                                             maximum_length, length_bucket_width=config.get("length_bucket_width",1), 
                                             multi_domain=config.get("multi_domain", True), picking_prob=None, 
                                             temperature=config.get("temperature",1.0), pick_in_order=True)
@@ -9624,7 +9625,7 @@ def train_L2W(config,
     loss = - tf.reduce_sum(tf.stop_gradient(tf.nn.softmax(domain_logits)) * tf.nn.log_softmax(domain_logits) * domain_rewards)
     if config.get("sampler_entropy_constraint",False):
       print("sampler_entropy_constraint_weight",config.get("sampler_entropy_constraint_weight",1e-3))
-      loss +=  config.get("sampler_entropy_constraint_weight",1e-3)* tf.nn.log_softmax(domain_logits)
+      loss +=  tf.reduce_sum(config.get("sampler_entropy_constraint_weight",1e-3)* tf.nn.log_softmax(domain_logits))
     grad = sampler_optimizer.get_gradients(loss,[domain_logits])
     grad_domain_logits_accum.assign_add(grad[0])
     return tf.reduce_sum(tf.stop_gradient(tf.nn.softmax(domain_logits)) * domain_rewards)
