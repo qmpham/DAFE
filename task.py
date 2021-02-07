09c1362d548003bd38a414abbda4a619b7f0a6c4
minhquangpham <phamminhquangnt07@gmail.com>
2021-02-07 16:29:35 +0100

    debug

--- task.py
+++ task.py
@@ -13074,9 +13074,10 @@ def debug_L2W_v1(config,
               for _ in range(config.get("train_batch_per_run_num",10)): 
                 src, tgt = next(train_iterators[i])
                 strategy.experimental_run_v2(_accumulate_dev_train_gradients, args=(src, tgt))
-              train_gradient_accumulator(sub_gradient_accumulator.gradients)
-              strategy.experimental_run_v2(_apply_dev_train_gradients)
-              #strategy.experimental_run_v2(sub_gradient_accumulator.reset)
+                strategy.experimental_run_v2(_apply_dev_train_gradients)
+                train_gradient_accumulator(sub_gradient_accumulator.gradients)
+              
+              strategy.experimental_run_v2(sub_gradient_accumulator.reset)
             ##### accumulate gradient over dev set of k tgt domains at theta_t+1
             with strategy.scope():
               for j, dev_iter in enumerate(dev_iterators):
