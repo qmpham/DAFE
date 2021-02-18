fb76bb9384eddeefa6ff6d889f0c3383903aaa45
minhquangpham <phamminhquangnt07@gmail.com>
2021-02-18 09:34:03 +0100

    debug

--- task.py
+++ task.py
@@ -11600,7 +11600,7 @@ def train_L2W_v2(config,
   if config.get("actor_parameterization","softmax") =="softmax":
     domain_logits = tf.Variable([0.0]*len(domain), trainable=True)
   elif config.get("actor_parameterization","softmax") =="linear":
-    domain_logits = tf.Variable([1.0/len(domain)]*len(domain), trainable=True)
+    domain_logits = tf.Variable(picking_prob, trainable=True)
   grad_domain_logits_accum = tf.Variable(tf.zeros_like(domain_logits), trainable=False)
   sampler_optimizer = tf.keras.optimizers.SGD(learning_rate=config.get("sampler_optim_lr",0.01)) #tf.keras.optimizers.Adam(learning_rate=config.get("sampler_optim_lr",0.01))
   sampler_vars = [domain_logits]
@@ -13256,7 +13256,283 @@ def debug_L2W_v2(config,
       sent_nums[i] += sum([len(lengths.numpy()) for lengths in src["length"].values])
   print("sent_nums: ",sent_nums)
 
+def train_IW_v0(config,
+          optimizer,          
+          learning_rate,
+          model,  
+          strategy,  
+          checkpoint_manager,
+          checkpoint,
+          adapter_optimizer=None,
+          checkpoint_path=None,
+          maximum_length=80,
+          batch_size = 2048,
+          batch_type = "tokens",
+          experiment="residual",
+          shuffle_buffer_size=-1,  # Uniform shuffle.
+          train_steps=200000,
+          save_every=5000,
+          eval_every=15000,
+          report_every=100): 
+  if config.get("train_steps",None)!=None:
+    train_steps = config.get("train_steps")
+  if config.get("batch_type",None)!=None:
+    batch_type = config.get("batch_type")
+  redistribute_every = config.get("redistribute_every",2000)
+  if config.get("use_meta_optimizer",False):
+    inner_optimizer = tf.keras.optimizers.SGD(config.get("meta_train_lr",0.001))
+  else:
+    inner_optimizer = optimizer
+  #####
+  if checkpoint_path is not None:
+    tf.get_logger().info("Restoring parameters from %s", checkpoint_path)
+    checkpoint.restore(checkpoint_path)
+  else:
+    if checkpoint_manager.latest_checkpoint is not None:
+      tf.get_logger().info("Restoring parameters from %s", checkpoint_manager.latest_checkpoint)
+      checkpoint.restore(checkpoint_manager.latest_checkpoint)    
+  #####
+  _summary_writer = tf.summary.create_file_writer(config["model_dir"])
+  #####
+  batch_train_size = config["batch_train_size"]  
+  batch_type = batch_type
+  source_file = config["src"]
+  target_file = config["tgt"]
+  domain = config.get("domain",None)
+  eval_domain = config.get("eval_domain")
+  ###### early stopping criterion
+  current_max_eval_bleu = 0.0
+  descending_streak = 0
+  ######
+  if not config.get("domain_importances",None):
+    domain_importances = [1.0/len(eval_domain)]*len(eval_domain)
+  else:
+    domain_importances = config.get("domain_importances")
+  print("There are %d in-domain corpora"%len(source_file))
+  ###############
+  print("cosine_reward: ",config.get("cosine_reward",True))
+  ###############
+  print("maximum_length", maximum_length)
+  train_dataset = create_trainining_dataset(strategy, model, domain, source_file, target_file, batch_train_size, batch_type, shuffle_buffer_size, 
+                                            maximum_length, length_bucket_width=config.get("length_bucket_width",1), 
+                                            multi_domain=config.get("multi_domain", True), picking_prob=config.get("picking_prob",None), temperature=config.get("temperature",1.0))
+  #############
+  dev_datasets = []
+  for d, source_file, target_file in zip(config.get("eval_domain"), config.get("eval_src"), config.get("eval_ref")):
+    dev_dataset = model.examples_inputter.make_training_dataset(source_file, target_file,
+              batch_size=batch_train_size//2,
+              batch_type="tokens",
+              domain=d,
+              single_pass=False,
+              shuffle_buffer_size=shuffle_buffer_size,
+              length_bucket_width=config.get("length_bucket_width",1),  # Bucketize sequences by the same length for efficiency.
+              maximum_features_length=None,
+              maximum_labels_length=None)
+    with strategy.scope():
+      base_dataset_ = dev_dataset
+      dev_dataset = strategy.experimental_distribute_datasets_from_function(
+          lambda _: base_dataset_)
+    dev_datasets.append(dev_dataset)
+  #############
+  with strategy.scope():
+    model.create_variables(optimizer=optimizer)
+    gradient_accumulator = optimizer_util.GradientAccumulator()  
+    sub_gradient_accumulator = optimizer_util.GradientAccumulator()
+    dev_gradient_accumulator = optimizer_util.GradientAccumulator()
 
+  def _accumulate_dev_train_gradients(source, target):
+    with tf.GradientTape() as tape:
+      variables = model.trainable_variables    
+      tape.watch(variables)
+      outputs, _ = model(
+          source,
+          labels=target,
+          training=True,
+          step=optimizer.iterations)
+      loss = model.compute_loss(outputs, target, training=True)
+
+      if isinstance(loss, tuple):
+        training_loss = loss[0] / loss[1]
+        reported_loss = loss[0] / loss[2]
+      else:
+        training_loss, reported_loss = loss, loss
+
+      gradients = tape.gradient(training_loss, variables)
+      sub_gradient_accumulator(gradients)
+      return training_loss
+
+  def _accumulate_gradients(source, target):
+    outputs, _ = model(
+        source,
+        labels=target,
+        training=True,
+        step=optimizer.iterations)
+    loss = model.compute_loss(outputs, target, training=True)
+    domain = source["domain"][0]
+    if isinstance(loss, tuple):
+      training_loss = loss[0] / loss[1]
+      reported_loss = loss[0] / loss[2]
+    else:
+      training_loss, reported_loss = loss, loss
+    
+    variables = model.trainable_variables
+    #print("var numb: ", len(variables))
+    """ for var in variables:
+      print(var.name) """
+    gradients = optimizer.get_gradients(training_loss, variables)
+    gradient_accumulator(gradients)
+    num_examples = tf.reduce_sum(target["length"])
+    #tf.summary.scalar("gradients/global_norm", tf.linalg.global_norm(gradients))    
+    return reported_loss, num_examples
+
+  def _apply_gradients():
+    variables = model.trainable_variables
+    grads_and_vars = []
+    for gradient, variable in zip(gradient_accumulator.gradients, variables):
+      # optimizer.apply_gradients will sum the gradients accross replicas.
+      scaled_gradient = gradient / (strategy.num_replicas_in_sync * tf.cast(gradient_accumulator.step, tf.float32))
+      grads_and_vars.append((scaled_gradient, variable))
+    optimizer.apply_gradients(grads_and_vars)
+    gradient_accumulator.reset()
+  
+  @dataset_util.function_on_next(train_dataset)
+  def _train_forward(next_fn):    
+    with strategy.scope():
+      per_replica_source, per_replica_target = next_fn()
+      per_replica_loss, per_replica_num_examples = strategy.experimental_run_v2(
+          _accumulate_gradients, args=(per_replica_source, per_replica_target))
+      # TODO: these reductions could be delayed until _step is called.
+      loss = strategy.reduce(tf.distribute.ReduceOp.MEAN, per_replica_loss, None)
+      num_examples = strategy.reduce(tf.distribute.ReduceOp.SUM, per_replica_num_examples, None)
+    return loss, num_examples
+  
+  @tf.function
+  def _step():
+    with strategy.scope():
+      strategy.experimental_run_v2(_apply_gradients)
+  
+  def _set_weight(v, w):
+    v.assign(tf.cast(w,v.dtype))
+
+  @tf.function
+  def weight_reset(snapshots):
+    with strategy.scope():
+      for snap, var in zip(snapshots, model.trainable_variables):
+        strategy.extended.update(var, _set_weight, args=(snap, ))
+
+  # Runs the training loop.
+  import time
+  start = time.time()  
+  train_data_flow = iter(_train_forward())
+  dev_iterators = [iter(dev_dataset) for dev_dataset in dev_datasets]
+ 
+  _, _ = next(train_data_flow)
+
+  print("number of replicas: %d"%strategy.num_replicas_in_sync)
+  print("accumulation step", config.get("accumulation_step",1))
+  _loss = []  
+  _number_examples = []
+  step = optimizer.iterations.numpy()
+  ########
+  excluded_params = []
+  for var in model.trainable_variables:
+    if tf.shape(var)[-1].numpy()==31266 or tf.shape(var)[0].numpy()==31266:
+      print(var.name)
+      excluded_params.append(var.name)
+  ########
+  if config.get("reset_step",None):
+    print("start from %d-th step"%config.get("reset_step",150000))
+    optimizer.iterations.assign(config.get("reset_step",150000))
+  
+  if step <= 1:
+    initializer = config.get("initializer","default")
+    if initializer == "default":
+      print("Initializing variables by tensorflow default")      
+    elif initializer == "variance_scaling":
+      print("Initializing variables by tf.variance_scaling")
+      initial_value = []
+      for v in model.trainable_variables:
+        shape = tf.shape(v).numpy()
+        initial_value.append(variance_scaling_initialier(shape, scale=1.0, mode="fan_avg", distribution="uniform"))
+      weight_reset(initial_value)   
+  
+  if config.get("continual_learning", False):
+    print("Continual Learning needs to load from old model")
+    assert config.get("checkpoint_path") != None
+    checkpoint_path = config.get("checkpoint_path")
+    load_and_update_if_needed_from_ckpt(config["model_dir"],   
+                        checkpoint_path,                        
+                        trackables={"model":model},
+                        vocab_update=True,
+                        model_key="model")
+
+  score_type = config.get("score_type","MultiBLEU")
+  if score_type == "sacreBLEU":
+    print("using sacreBLEU")
+    scorer = BLEUScorer()
+  elif score_type == "MultiBLEU":
+    print("using MultiBLEU")
+    scorer = MultiBLEUScorer()
+  ref_eval_concat = file_concatenate(config["eval_ref"],"ref_eval_concat",dir_name=os.path.join(config["model_dir"],"eval"))
+  
+  with _summary_writer.as_default():
+    while True:
+      ####Training batch
+      loss, num_examples = next(train_data_flow)    
+      _loss.append(loss.numpy())
+      _number_examples.append(num_examples.numpy())
+      _step()  
+      step = optimizer.iterations.numpy()
+      
+      if step % redistribute_every == 0 and step > config.get("warm_start",5000):
+        #######        
+        with strategy.scope():
+          for j, dev_iter in enumerate(dev_iterators):
+            for _ in range(config.get("dev_batch_per_run_num",10)):
+              src, tgt = next(dev_iter)
+              strategy.experimental_run_v2(_accumulate_dev_train_gradients, args=(src, tgt))
+            dev_gradient_accumulator(sub_gradient_accumulator.gradients)
+          strategy.experimental_run_v2(sub_gradient_accumulator.reset)         
+              
+
+      if step % report_every == 0:
+        elapsed = time.time() - start
+        tf.get_logger().info(
+          "Step = %d ; Learning rate = %f ; Loss = %f; number_examples = %d, after %f seconds",
+          step, learning_rate(step), np.mean(_loss), np.sum(_number_examples), elapsed)
+        _loss = []
+        _number_examples = []
+        start = time.time()
+      if step % save_every == 0:
+        tf.get_logger().info("Saving checkpoint for step %d", step)
+        checkpoint_manager.save(checkpoint_number=step)
+      if step % eval_every == 0:
+        checkpoint_path = checkpoint_manager.latest_checkpoint
+        tf.summary.experimental.set_step(step)
+        output_files = []
+        new_bleu = 0.0
+        for src,ref,i in zip(config["eval_src"],config["eval_ref"],config["eval_domain"]):
+          output_file = os.path.join(config["model_dir"],"eval",os.path.basename(src) + ".trans." + os.path.basename(checkpoint_path))
+          score = translate(src, ref, model, checkpoint_manager, checkpoint, i, output_file, length_penalty=config.get("length_penalty",0.6), experiment=experiment)
+          tf.summary.scalar("eval_score_%d"%i, score, description="BLEU on test set %s"%src)
+          output_files.append(output_file)
+          new_bleu += score * domain_importances[i]
+        ##### BLEU on concat dev set.
+        output_file_concat = file_concatenate(output_files,"output_file_concat.%s"%os.path.basename(checkpoint_path))
+        score = scorer(ref_eval_concat, output_file_concat)
+        print("score of model %s on concat dev set: "%checkpoint_manager.latest_checkpoint, score)
+        tf.summary.scalar("concat_eval_score", score, description="BLEU on concat dev set")
+        #############################
+        if new_bleu >= current_max_eval_bleu:
+          current_max_eval_bleu = new_bleu
+          descending_streak = 0
+        else:
+          descending_streak += 1
+      tf.summary.flush()
+      if config.get("early_stopping",True) and descending_streak >= 5:
+        break
+      if step > train_steps:
+        break
 
 
 
