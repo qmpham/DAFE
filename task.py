d1d50a558ff6650f5f2513dfc1433c692c14ba84
minhquangpham <phamminhquangnt07@gmail.com>
2021-03-03 01:09:15 +0100

    debug

--- task.py
+++ task.py
@@ -13776,21 +13776,21 @@ def debug_L2W_v2(config,
                                             multi_domain=config.get("multi_domain", True), picking_prob=None, temperature=config.get("temperature",1.0))
                                             for domain, source_file, target_file in zip(config.get("domain"), config.get("src"), config.get("tgt"))]
 
-  # dev_datasets = [create_trainining_dataset(strategy, model, [domain], [source_file], [target_file], batch_train_size//2, batch_type, shuffle_buffer_size, 
-  #                                           maximum_length, length_bucket_width=config.get("length_bucket_width",1), 
-  #                                           multi_domain=config.get("multi_domain", True), picking_prob=None, temperature=config.get("temperature",1.0))
-  #                                           for domain, source_file, target_file in zip(config.get("eval_domain"), config.get("eval_src"), config.get("eval_ref"))]
-  dev_datasets = []
-  for d, source_file, target_file in zip(config.get("eval_domain"), config.get("eval_src"), config.get("eval_ref")):
-    dev_dataset = model.examples_inputter.make_training_dataset(source_file, target_file,
-              batch_size=25,
-              batch_type="examples",
-              domain=d,
-              single_pass=True,
-              shuffle_buffer_size=None,
-              length_bucket_width=config.get("length_bucket_width",1),  # Bucketize sequences by the same length for efficiency.
-              maximum_features_length=None,
-              maximum_labels_length=None)
+  dev_datasets = [create_trainining_dataset(strategy, model, [domain], [source_file], [target_file], batch_train_size//2, batch_type, shuffle_buffer_size, 
+                                            maximum_length, length_bucket_width=config.get("length_bucket_width",1), 
+                                            multi_domain=config.get("multi_domain", True), picking_prob=None, temperature=config.get("temperature",1.0))
+                                            for domain, source_file, target_file in zip(config.get("eval_domain"), config.get("eval_src"), config.get("eval_ref"))]
+  # dev_datasets = []
+  # for d, source_file, target_file in zip(config.get("eval_domain"), config.get("eval_src"), config.get("eval_ref")):
+  #   dev_dataset = model.examples_inputter.make_training_dataset(source_file, target_file,
+  #             batch_size=25,
+  #             batch_type="examples",
+  #             domain=d,
+  #             single_pass=True,
+  #             shuffle_buffer_size=None,
+  #             length_bucket_width=config.get("length_bucket_width",1),  # Bucketize sequences by the same length for efficiency.
+  #             maximum_features_length=None,
+  #             maximum_labels_length=None)
     with strategy.scope():
       base_dataset_ = dev_dataset
       dev_dataset = strategy.experimental_distribute_datasets_from_function(
