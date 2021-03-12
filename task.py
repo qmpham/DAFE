4a6573a1bc99963108d2c567fce7d0b6b48b0646
minhquangpham <phamminhquangnt07@gmail.com>
2021-03-12 10:55:46 +0100

    debug

--- task.py
+++ task.py
@@ -13776,8 +13776,9 @@ def debug_L2W_v1(config,
                 # reset train dev gradient accumulations to zero
                 strategy.experimental_run_v2(train_gradient_accumulator.reset)
               #######
-              print("reward of training set %d to dev set %d: %f"%(i,j,reward_[i,j]))
-              rewards_acc.append(reward_[i,j])
+              for j, dev_iter in enumerate(dev_iterators):
+                print("reward of training set %d to dev set %d: %f"%(i,j,reward_[i,j]))
+                rewards_acc.append(reward_[i,j])
               ####### loss of dev batch at theta_t+1
               for src, tgt in dev_batches[j]:
                 loss_per_device = strategy.experimental_run_v2(_compute_loss, args=(src, tgt))
