0db97c9ec365dc88aea5fac233eab3d4202ecc04
minhquangpham <phamminhquangnt07@gmail.com>
2021-06-23 10:05:52 +0200

    debug

--- practice.py
+++ practice.py
@@ -84,12 +84,12 @@ def main():
     gpus = tf.config.list_physical_devices('GPU')
     if args.gpus_num:
       assert int(args.gpus_num) <= len(gpus)
-      gpus = tf.config.list_physical_devices('GPU')[:int(args.gpus_num)]
+      gpus = tf.config.list_physical_devices('GPU')[0:int(args.gpus_num)]
       print("using %d over %d found gpus"%(int(args.gpus_num), len(tf.config.list_physical_devices('GPU'))))
     for gpu in gpus:
       tf.config.experimental.set_memory_growth(gpu, True)
-    #devices = tf.config.experimental.list_logical_devices(device_type="GPU")
-    strategy = tf.distribute.MirroredStrategy(devices=[d.name for d in gpus])
+    devices = tf.config.experimental.list_logical_devices(device_type="GPU")[0:int(args.gpus_num)]
+    strategy = tf.distribute.MirroredStrategy(devices=[d.name for d in devices])
     if not os.path.exists(os.path.join(config["model_dir"],"eval")):
       os.makedirs(os.path.join(config["model_dir"],"eval"))
 
