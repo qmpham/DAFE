7a1302e0156d31e8ab4b34eef9144e8bce480611
minhquangpham <phamminhquangnt07@gmail.com>
2021-08-23 21:58:48 +0200

    debug

--- layers/layers.py
+++ layers/layers.py
@@ -1788,6 +1788,7 @@ class Multi_domain_classification_gate(tf.keras.layers.Layer):
     #tf.print("logits 3: ", logits, summarize=1000)
     #tf.print("%s outputs: "%(self.name_scope()), tf.math.softmax(logits),summarize=1000)
     outputs = tf.math.softmax(logits)
+    tf.print("domain_logits:",tf.shape(outputs), outputs)
     outputs = tf.tile(tf.expand_dims(outputs,1),[1,self.output_dim,1])
     tf.print("domain_logits:",tf.shape(outputs), outputs)
     print("input_shape:", shape)
