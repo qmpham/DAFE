6566494b5067b0e3eef98afb4ee7dd0703e8e3b8
minhquangpham <phamminhquangnt07@gmail.com>
2021-08-23 18:12:51 +0200

    debug

--- decoders/self_attention_decoder.py
+++ decoders/self_attention_decoder.py
@@ -5422,7 +5422,7 @@ class Multi_domain_SelfAttentionDecoder_v17(Decoder):
           cache=cache[i] if cache is not None else None,
           training=training)
       new_cache.append(layer_cache)
-      if self.version not in [3,8,9,10,11,12,15,16]:
+      if self.version not in [3,8,9,10,11,12,15,16,17]:
         adapt = multi_domain_layer(inputs, domain, mask=mask, training=training)
         total_adapt.append(adapt)
       if self.version == 17:
@@ -5449,11 +5449,11 @@ class Multi_domain_SelfAttentionDecoder_v17(Decoder):
         lhuc_scale = 2 * tf.math.sigmoid(lhuc_vector)
         inputs = tf.math.multiply(inputs, lhuc_scale) + inputs
 
-    if self.version not in [3,8,9,10,11,12,15,16]:
+    if self.version not in [3,8,9,10,11,12,15,16,17]:
       total_adapt = tf.add_n(total_adapt)
     elif self.version in [8,9]:
       total_adapt = self.multi_domain_layers[-1](inputs, domain, mask=mask, training=training)
-    if self.version not in [3,7,9,10,11,12,15,16]:
+    if self.version not in [3,7,9,10,11,12,15,16,17]:
       g = self.multi_domain_gate(inputs, domain, mask=mask, training=training)
     if self.version == 17:
       g = self.multi_domain_gate.domain_logits(inputs, mask=mask, training=training)
