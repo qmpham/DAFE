127bc166655e21fd75e28d54830545b97bd1404a
minhquangpham <phamminhquangnt07@gmail.com>
2021-08-23 22:03:02 +0200

    debug

--- layers/layers.py
+++ layers/layers.py
@@ -1789,7 +1789,7 @@ class Multi_domain_classification_gate(tf.keras.layers.Layer):
     #tf.print("%s outputs: "%(self.name_scope()), tf.math.softmax(logits),summarize=1000)
     outputs = tf.math.softmax(logits)
     tf.print("domain_logits:",tf.shape(outputs), outputs)
-    outputs = tf.tile(tf.expand_dims(outputs,1),[1,self.output_dim,1])
+    outputs = tf.tile(tf.expand_dims(outputs,-1),[1,1,self.output_dim])
     tf.print("domain_logits:",tf.shape(outputs), outputs)
     print("input_shape:", shape)
     if rank > 2:
