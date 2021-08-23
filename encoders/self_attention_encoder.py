816487748a0229f91996d4294f34754e4501889c
minhquangpham <phamminhquangnt07@gmail.com>
2021-08-23 18:19:58 +0200

    debug

--- encoders/self_attention_encoder.py
+++ encoders/self_attention_encoder.py
@@ -1586,7 +1586,7 @@ class Multi_domain_SelfAttentionEncoder_v15(Encoder):
       total_adapt = tf.add_n(total_adapt)
     elif self.version in [8,9]:
       total_adapt = self.multi_domain_layers[-1](inputs, domain, mask=mask, training=training)
-    if self.version not in [3,7,9,10,11,12,15,16],17:
+    if self.version not in [3,7,9,10,11,12,15,16,17]:
       g = self.multi_domain_gate(inputs, domain, mask=mask, training=training)
       if internal_node_printing:
         tf.print("###", self.name_scope(), "gate_mean_abs_pooling: ", tf.reduce_mean(g,-1)[0,:], "domain: ", domain, "###", sep="|", summarize=1000)
