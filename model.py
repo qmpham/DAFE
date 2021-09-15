5800b090c81f7e865fba1ac67e81ac91893ab64f
minhquangpham <phamminhquangnt07@gmail.com>
2021-09-15 14:28:46 +0200

    debug

--- model.py
+++ model.py
@@ -3413,11 +3413,11 @@ class Multi_domain_SequenceToSequence_sparse(model.SequenceGenerator):
       KL_term = tf.reduce_sum((1-dropout_rate) * tf.math.log(prob_one) + dropout_rate * tf.math.log(prob_zero))
     
     if training:
-      domain_dropout_mask = tf.concat([tf.ones(self.num_units - self.num_shared_units),tf.cast(tf.reshape(tf.transpose(tf.tile(tf.expand_dims(prob_one,0),[self.unit_group_size,1])),[-1]),tf.float32)],-1)
+      domain_dropout_mask = tf.concat([tf.ones(self.num_shared_units),tf.cast(tf.reshape(tf.transpose(tf.tile(tf.expand_dims(prob_one,0),[self.unit_group_size,1])),[-1]),tf.float32)],-1)
       #tf.print("domain_dropout_mask",domain_dropout_mask,summarize=-1)
       #tf.print("gumbel_temperature",gumbel_temperature,summarize=-1)
     else:
-      domain_dropout_mask = tf.concat([tf.ones(self.num_units - self.num_shared_units),tf.cast(tf.reshape(tf.transpose(tf.tile(tf.expand_dims(tf.math.argmax(unit_selection_logits,1),0),[self.unit_group_size,1])),[-1]),tf.float32)],-1)
+      domain_dropout_mask = tf.concat([tf.ones(self.num_shared_units),tf.cast(tf.reshape(tf.transpose(tf.tile(tf.expand_dims(tf.math.argmax(unit_selection_logits,1),0),[self.unit_group_size,1])),[-1]),tf.float32)],-1)
 
     encoder_outputs, encoder_state, encoder_sequence_length = self.encoder(
         [source_inputs, features["domain"], domain_dropout_mask], sequence_length=source_length, training=training)
