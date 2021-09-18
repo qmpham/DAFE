390ee239f774a1407a1911ff7a8613caadc4ad1d
minhquangpham <phamminhquangnt07@gmail.com>
2021-09-18 11:37:23 +0200

    debug

--- task.py
+++ task.py
@@ -17215,7 +17215,7 @@ def train_elbo_sparse_layer(config,
   print("gumbel_temperature_decay",gumbel_temperature_decay)
   print("r_coeff",r)
   step = optimizer.iterations.numpy()
-  gumbel_temperature.assign(tf.cast(tf.math.maximum(min_temperature, tf.math.exp(-r*step)),tf.float32))
+  gumbel_temperature.assign(tf.cast(tf.math.maximum(min_temperature, tf.math.exp(-0.5*r*step)),tf.float32))
   print("gumbel_temperature: ",gumbel_temperature)
   with _summary_writer.as_default():
     while True:
