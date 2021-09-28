7fb9429c05163ce1070696334f04c81fd5540c3b
minhquangpham <phamminhquangnt07@gmail.com>
2021-09-28 17:17:53 +0200

    debug

--- task.py
+++ task.py
@@ -17503,12 +17503,6 @@ def train_elbo_topK_sparse_layer(config,
     deltaL_deltaM = gradient_soft_mask[0] # in R^n_g
 
     tf.print("gradient_soft_mask",gradient_soft_mask[0],summarize=-1)
-    tf.tile(tf.expand_dims(domain_allocation_probs,1),[1,model.num_domain_unit_group])
-    tf.tile(tf.expand_dims(domain_allocation_probs,0),[model.num_domain_unit_group,1]) 
-    tf.linalg.diag(-tf.ones(model.num_domain_unit_group)) + 1
-    optimizer.get_gradients(training_loss,[model.latent_group_allocation_logit])
-    tf.print("probs/latent",optimizer.get_gradients(domain_allocation_probs,[model.latent_group_allocation_logit]),summarize=-1)
-    #tf.print("softmask/latent", optimizer.get_gradients(soft_mask,[model.latent_group_allocation_logit]),summarize=-1)
     M1 = tf.linalg.diag(tf.math.square(tf.math.sigmoid((gumbel_sample+domain_allocation_probs+temp_x)/temperature))/tf.math.exp((gumbel_sample+domain_allocation_probs+temp_x)/temperature))
     #deltaSoftMax_deltaLogit = g.jacobian(domain_allocation_probs,latent_group_allocation_logit)
     #tf.print("deltaSoftMax_deltaLogit",deltaSoftMax_deltaLogit,summarize=-1)
