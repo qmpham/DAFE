13f04e942232cd60e1802dce69bd4c0d1dfd546b
minhquangpham <phamminhquangnt07@gmail.com>
2022-03-24 11:53:49 +0100

    debug

--- layers/common.py
+++ layers/common.py
@@ -286,10 +286,11 @@ class LayerWrapper_v2(tf.keras.layers.Layer):
     
     training = kwargs.get("training")
     x = inputs
+    tf.print("inner self attention before norm: ", x[0,0,:], summarize=-1)
     if self.input_layer_norm is not None:
       x = self.input_layer_norm(x)  # pylint: disable=not-callable
     x = dropout(x, self.input_dropout, training=training)
-    tf.print("inner self attention: ", x[0,0,:], summarize=-1)
+    tf.print("inner self attention after norm: ", x[0,0,:], summarize=-1)
     all_outputs = self.layer(x, *args, **kwargs)
     if isinstance(all_outputs, tuple):
       outputs = all_outputs[0]
